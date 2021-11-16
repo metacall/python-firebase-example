1bd83602ff9ebf2a6e5b44636f651bc2de641a82
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2021-11-16 21:18:15 +0100

    Change bucket to firestore.

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import storage
-from os.path import join, dirname, abspath
+from firebase_admin import firestore
+import os
 
 # Set up credentials
-cred = credentials.Certificate(join(dirname(abspath(__file__)), 'service-account.json'))
+cred = credentials.Certificate(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'service-account.json'))
 
 firebase_admin.initialize_app(cred, {
-    'storageBucket': os.environ['STORAGE_ID'] + '.appspot.com'
+    'storageBucket': os.environ['PROJECT_ID'] + '.appspot.com'
 })
 
-bucket = storage.bucket(os.environ['BUCKET_ID'])
+firestore_db = firestore.client()
 
 def query():
-    return list(bucket.list_blobs())
+    snapshots = list(firestore_db.collection(os.environ['TABLE_ID']).get())
+
+    result = []
+
+    for snapshot in snapshots:
+        result.append(snapshot.to_dict())
+
+    return result
